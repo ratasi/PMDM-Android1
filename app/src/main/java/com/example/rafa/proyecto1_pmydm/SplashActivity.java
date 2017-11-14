9855c383f8b4a7d15a28e3fca612259ccd1f842e
Rafa <rafatarre@gmail.com>
2017-11-14 15:58:45 +0100

    subiendo proyecto

---
+++ app/src/main/java/com/example/rafa/proyecto1_pmydm/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.rafa.proyecto1_pmydm;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import java.util.*;
+
+/**
+ * Created by Rafa on 13/11/2017.
+ */
+
+public class SplashActivity {/*
+        private long SPLASH_DELAY = 6000; //6 segundos
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_splash);
+            //TimerTask
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    Intent mainIntent = new Intent(getApplicationContext(),
+                            MainActivity.class);
+                    startActivity(mainIntent);
+                    //Destruimos esta activity para prevenir
+                    // que el usuario vuelva a este Activity presionando el boton
+                    // Atras.
+                    finish();
+                }
+            };
+            Timer timer = new Timer();
+            timer.schedule(task, SPLASH_DELAY);
+        }*/
+    }
+
